19350a0c789efae306fead9eb5c0cec6e0643c62
Tydyannes <123898524+Tydyannes@users.noreply.github.com>
2023-02-06 17:17:50 +0100

    Create TP2.R

---
+++ TP2/TP2.R
@@ -0,0 +1,113 @@
+# Exercice 1 : Importer les données : 
+
+# a. Importez le jeu de données pokemon.xlsx à l’aide du package readxl.
+
+install.packages("readxl")
+library(readxl)
+setwd("~/GitHub/programmation_r/TP2")
+df <- read_excel( path = "pokemon.xlsx", sheet = "pokemon")
+
+# b. Combien de lignes, colonnes sont présentes dans ce dataset (utilisez les fonctions adaptées) ?
+
+nrow(df)
+ncol(df)
+
+#   c. Affichez le nom des colonnes.
+
+colnames(df)
+
+# d. Affichez le type des colonnes avec la fonction adaptée.
+
+str(df)
+
+# e. On souhaite analyser les variables generation, is_legendary, et type en tant que variables qualitatives. Modifier le type de ces variables pour les transformer en type factor.
+
+df$is_legendary <- as.factor(df$is_legendary)
+df$generation <- as.factor(df$generation)
+df$type <- as.factor(df$type)
+
+# f. Combien de niveaux (levels) sont présents dans ces variables ?
+
+length(levels(df$is_legendary))
+length(levels(df$generation))
+length(levels(df$type))
+
+#   g. Affichez un résumé des données avec la fonction adaptée.
+
+summary(df)
+
+
+# Exercice 2 : Statistiques descriptives : 
+
+# a. Déterminer la moyenne de la variable weight_kg.
+
+mean(df$weight_kg, na.rm = T)
+
+# b. Déterminer la médiane de la variable weight_kg.
+
+median(df$weight_kg, na.rm = T)
+
+# c. Déterminer les quartiles de la variable height_m.
+
+quantile(df$height_m, na.rm = T)
+
+# d. Déterminer les déciles de la variable height_m.
+
+quantile(df$height_m, seq(0, 1, 0.1), na.rm = T)
+
+# e. Déterminer la variance et l’écart-type de la variable weight_kg.
+
+var(df$weight_kg, na.rm = T)
+sd(df$weight_kg, na.rm = T)
+
+# f. Déterminer un tri à plat pour compter les effectifs des modalités de chaque variable factor en triant
+# chaque sortie par ordre décroissant.
+
+sort(table(df$is_legendary), decreasing = T)
+sort(table(df$generation), decreasinf = T)
+sort(table(df$type), decreasing = T)
+
+
+# Exercice 3 : Tris et Selections
+
+# a. Sélectionnez la colonne nom et is_legendary.
+
+requete0 <- df[c("nom","is_legendary")]
+dim(requete0)
+
+# b. Sélectionnez les 50 premières lignes et les deux premières colonnes.
+
+requete1 <- df[1:50,1:2]
+dim(requete1)
+
+# c. Sélectionnez les 10 premières lignes et toutes les colonnes.
+
+requete2 <- df[1:10,]
+dim(requete2)
+
+# d. Sélectionnez toutes les colonnes sauf la dernière.
+
+requete3 <- df[-10]
+dim(requete3)
+
+# e. Triez le dataset par ordre alphabétique et afficher le nom du pokemon dela première ligne.
+
+requete4 <- order(df$nom,decreasing = F)
+df <- df[requete4,]
+requete5 <- df$nom[1]
+requete5
+
+# f. Triez le dataset par weight_kg en ordre décroissant, et afficher le nom du pokemon de la première ligne
+
+requete6 <- order(df$weight_kg, decreasing = T)
+df <- df[requete6,]
+requete7 <- df$nom[1]
+requete7
+
+# g. Triez le dataset par attack en ordre décroissant puis par speed en ordre croissant, et afficher le
+# nom des pokemons des 10 premières lignes.
+
+requete8 <- order(-df$attack,df$speed)
+df <- df[requete8,]
+requete10 <- df$nom[1:10]
+requete10
